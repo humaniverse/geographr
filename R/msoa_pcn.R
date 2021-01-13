b91b81e24cafaaa3a45dbfbf2313e8fe85b83e37
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-01-13 13:02:07 +0100

    Initiate file

---
+++ R/msoa_pcn.R
@@ -0,0 +1,8 @@
+msoa_pcn <- function(.data){
+
+}
+
+# https://github.com/britishredcrosssociety/covid-19-vulnerability/tree/master/data/primary-care-networks
+# https://github.com/britishredcrosssociety/covid-19-vulnerability/blob/master/data/lookup%20msoa%20to%20primary%20care%20network.csv
+# Can the postcodes of the PCN's be used to map to MSOA's, accounting for overlaps
+# by assuming a set radius of n for each PCN?
