51627fc50db2d7d4f4129ea11da6fe471c614a41
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-02-04 13:08:27 +0100

    Aggregate using areal-weighted spatial interpolation

--- R/aggregate.R
+++ R/aggregate.R
@@ -1,2 +1,21 @@
 # ---- MSOA to PCN ----
-# https://geocompr.robinlovelace.net/spatial-operations.html#spatial-aggr
+# Borders are incongruent
+# Use areal-weighted spatial interpolation
+# Source: https://geocompr.robinlovelace.net/spatial-operations.html#spatial-aggr
+msoa_pcn <- function(metric, extensive = TRUE) {
+  sf::st_interpolate_aw(
+    geographr::msoa_11[, metric],
+    geographr::pcn,
+    extensive = extensive
+  )
+}
+
+msoa_temp <-
+  read_sf("https://raw.githubusercontent.com/britishredcrosssociety/covid-19-vulnerability/master/output/vulnerability-MSOA-England.geojson") %>%
+  st_make_valid()
+
+sf::st_interpolate_aw(
+  msoa_temp[, "Vulnerability.quintile"],
+  sf::st_make_valid(geographr::pcn),
+  extensive = FALSE
+)
