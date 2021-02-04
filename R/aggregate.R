c12c3757f9588d4e4e12d17f90c89bec39a80b43
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-02-04 13:33:44 +0100

    Add notes for dummy example

--- R/aggregate.R
+++ R/aggregate.R
@@ -10,12 +10,22 @@ msoa_pcn <- function(metric, extensive = TRUE) {
   )
 }
 
-msoa_temp <-
-  read_sf("https://raw.githubusercontent.com/britishredcrosssociety/covid-19-vulnerability/master/output/vulnerability-MSOA-England.geojson") %>%
-  st_make_valid()
+# Notes:
+# - The above function will fail. The msoa_11 data set imported needs combining
+#   with a metric that we wish to aggregate (e.g., vulnerability score)
+# - Uncomment the dummy example below that uses the msoa_11 dataset with VI
+#   scores appended. Ignore the warning errors and notice the new PCN
+#   vulnerability scores that are calculated:
 
-sf::st_interpolate_aw(
-  msoa_temp[, "Vulnerability.quintile"],
-  sf::st_make_valid(geographr::pcn),
-  extensive = FALSE
-)
+# library(tidyverse)
+# library(sf)
+#
+# msoa_temp <-
+#   read_sf("https://raw.githubusercontent.com/britishredcrosssociety/covid-19-vulnerability/master/output/vulnerability-MSOA-England.geojson") %>%
+#   st_make_valid()
+#
+# sf::st_interpolate_aw(
+#   msoa_temp[, "Vulnerability.quintile"],
+#   sf::st_make_valid(geographr::pcn),
+#   extensive = FALSE
+# )
