457434e86120c1f7a1a8c02761248d76c87029b2
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-02-09 15:52:34 +0100

    Comment map to allow build

--- R/aggregate.R
+++ R/aggregate.R
@@ -30,81 +30,84 @@ msoa_pcn <- function(metric, extensive = TRUE) {
 #   extensive = FALSE
 # )
 
-library(tidyverse)
-library(htmlwidgets)
-library(sf)
-library(leaflet)
-library(htmltools)
+# library(tidyverse)
+# library(htmlwidgets)
+# library(sf)
+# library(leaflet)
+# library(htmltools)
+#
+# # Create Title
+# tag.map.title <- tags$style(HTML("
+#   .leaflet-control.map-title {
+#     transform: translate(-50%,20%);
+#     position: fixed !important;
+#     left: 50%;
+#     text-align: center;
+#     padding-left: 10px;
+#     padding-right: 10px;
+#     background: rgba(255,255,255,1);
+#     font-weight: bold;
+#     font-size: 22px;
+#   }
+# "))
+#
+# title <- tags$div(
+#   tag.map.title, HTML("Exploe MSOA and PCN boundary overlaps")
+# )
+#
+# map <-
+#   leaflet(
+#   options = leafletOptions(minZoom = 5, maxZoom = 15, attributionControl = F)
+# ) %>%
+#   setView(lat = 54.00366, lng = -2.547855, zoom = 7) %>%
+#   addProviderTiles(providers$CartoDB.Positron) %>%
+#   addMapPane("msoa", zIndex = 410) %>%
+#   addMapPane("pcn", zIndex = 420) %>%
+#   addPolygons(
+#     data = geographr::msoa_2011,
+#     fillColor = "#73aeea",
+#     weight = 0.7,
+#     opacity = 0.8,
+#     color = "black",
+#     dashArray = "2",
+#     fillOpacity = 0.7,
+#     options = pathOptions(pane = "msoa"),
+#     highlight = highlightOptions(
+#       weight = 5,
+#       color = "#666",
+#       dashArray = "",
+#       fillOpacity = 0.7,
+#       bringToFront = TRUE
+#     ),
+#     label = ~msoa_name,
+#     group = "msoa"
+#   ) %>%
+#   addPolygons(
+#     data = st_collection_extract(geographr::pcn, "POLYGON"),
+#     fillColor = "#bb7ed7",
+#     weight = 0.7,
+#     opacity = 0.8,
+#     color = "black",
+#     dashArray = "2",
+#     fillOpacity = 0.7,
+#     options = pathOptions(pane = "pcn"),
+#     highlight = highlightOptions(
+#       weight = 5,
+#       color = "#666",
+#       dashArray = "",
+#       fillOpacity = 0.7,
+#       bringToFront = TRUE
+#     ),
+#     label = ~pcn_name,
+#     group = "pcn"
+#   ) %>%
+#   addLayersControl(
+#     overlayGroups = c("msoa", "pcn"),
+#     options = layersControlOptions(collapsed = FALSE)
+#   ) %>%
+#   addControl(title, position = "topright", className="map-title")
+#
+# saveWidget(map, "misc/map.html")
 
-# Create Title
-tag.map.title <- tags$style(HTML("
-  .leaflet-control.map-title {
-    transform: translate(-50%,20%);
-    position: fixed !important;
-    left: 50%;
-    text-align: center;
-    padding-left: 10px;
-    padding-right: 10px;
-    background: rgba(255,255,255,1);
-    font-weight: bold;
-    font-size: 22px;
-  }
-"))
+# ---- MSOA to CCG ----
 
-title <- tags$div(
-  tag.map.title, HTML("Exploe MSOA and PCN boundary overlaps")
-)
-
-map <-
-  leaflet(
-  options = leafletOptions(minZoom = 5, maxZoom = 15, attributionControl = F)
-) %>%
-  setView(lat = 54.00366, lng = -2.547855, zoom = 7) %>%
-  addProviderTiles(providers$CartoDB.Positron) %>%
-  addMapPane("msoa", zIndex = 410) %>%
-  addMapPane("pcn", zIndex = 420) %>%
-  addPolygons(
-    data = geographr::msoa_2011,
-    fillColor = "#73aeea",
-    weight = 0.7,
-    opacity = 0.8,
-    color = "black",
-    dashArray = "2",
-    fillOpacity = 0.7,
-    options = pathOptions(pane = "msoa"),
-    highlight = highlightOptions(
-      weight = 5,
-      color = "#666",
-      dashArray = "",
-      fillOpacity = 0.7,
-      bringToFront = TRUE
-    ),
-    label = ~msoa_name,
-    group = "msoa"
-  ) %>%
-  addPolygons(
-    data = st_collection_extract(geographr::pcn, "POLYGON"),
-    fillColor = "#bb7ed7",
-    weight = 0.7,
-    opacity = 0.8,
-    color = "black",
-    dashArray = "2",
-    fillOpacity = 0.7,
-    options = pathOptions(pane = "pcn"),
-    highlight = highlightOptions(
-      weight = 5,
-      color = "#666",
-      dashArray = "",
-      fillOpacity = 0.7,
-      bringToFront = TRUE
-    ),
-    label = ~pcn_name,
-    group = "pcn"
-  ) %>%
-  addLayersControl(
-    overlayGroups = c("msoa", "pcn"),
-    options = layersControlOptions(collapsed = FALSE)
-  ) %>%
-  addControl(title, position = "topright", className="map-title")
-
-saveWidget(map, "misc/map.html")
