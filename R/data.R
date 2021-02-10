1978ac1e9729d4ef305c022a4b9bb52931bc4c9d
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-02-10 15:42:44 +0100

    Add boundary info

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' Primary care networks
 #'
-#' A data set containing Primary Care Network (PCN) geometries calculated using their
-#' member GP practices.
+#' A data set containing Primary Care Network (PCN) geometries calculated using
+#' their member GP practices.
 #'
 #' @format A data frame of class "sf" with 1,278 rows and 3 variables:
 #' \describe{
@@ -12,6 +12,38 @@
 #' }
 #' @source \url{https://digital.nhs.uk/}
 "pcn"
+
+#' Clinical Commissioning Groups (April 2020)
+#'
+#' A data set containing 2020 Clinical Commissioning Group (CCG) geometries and
+#' their associated names and codes. Boundaries are ultra generalised (500m).
+#'
+#' @format A data frame of class "sf" with 135 rows and 3 variables:
+#' \describe{
+#'   \item{ccg_code}{CCG code}
+#'   \item{ccg_name}{CCG name}
+#'   \item{geometry}{multipolygon coordinates}
+#'   ...
+#' }
+#' @source \url{https://geoportal.statistics.gov.uk/}
+"ccg"
+
+#' Sustainability and Transformation Partnerships (April 2020)
+#'
+#' A data set containing 2020 Sustainability and Transformation Partnerships
+#' (STP) geometries and their associated names and codes. Boundaries are ultra
+#' generalised (500m).
+#'
+#' @format A data frame of class "sf" with 42 rows and 3 variables:
+#' \describe{
+#'   \item{stp_code}{STP code}
+#'   \item{stp_name}{STP name}
+#'   \item{geometry}{multipolygon coordinates}
+#'   ...
+#' }
+#' @source \url{https://geoportal.statistics.gov.uk/}
+"stp"
+
 
 #' Middle Super Output Areas (2011)
 #'
@@ -27,33 +59,3 @@
 #' }
 #' @source \url{https://geoportal.statistics.gov.uk/}
 "msoa_2011"
-
-#' Clinical Commissioning Groups (April 2020)
-#'
-#' A data set containing 2020 Clinical Commissioning Group (CCG) geometries and
-#' their associated names and codes.
-#'
-#' @format A data frame of class "sf" with 135 rows and 3 variables:
-#' \describe{
-#'   \item{ccg_code}{CCG code}
-#'   \item{ccg_name}{CCG name}
-#'   \item{geometry}{multipolygon coordinates}
-#'   ...
-#' }
-#' @source \url{https://geoportal.statistics.gov.uk/}
-"ccg"
-
-#' Sustainability and Transformation Partnerships (April 2020)
-#'
-#' A data set containing 2020 Sustainability and Transformation Partnerships
-#' (STP) geometries and their associated names and codes.
-#'
-#' @format A data frame of class "sf" with 42 rows and 3 variables:
-#' \describe{
-#'   \item{stp_code}{STP code}
-#'   \item{stp_name}{STP name}
-#'   \item{geometry}{multipolygon coordinates}
-#'   ...
-#' }
-#' @source \url{https://geoportal.statistics.gov.uk/}
-"stp"
