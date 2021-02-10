80fbc1947fa4255076ee3c798e2da9fd5f3a0587
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-02-10 16:10:31 +0100

    Replace msoa with raw source

--- data-raw/msoa.R
+++ data-raw/msoa.R
@@ -1,25 +1,41 @@
 # ---- Load libraries ----
 library(tidyverse)
 library(sf)
+library(httr)
+library(rmapshaper)
 
 # ---- Load shapefile ----
-# Middle Layer Super Output Areas (December 2011) Boundaries EW BFC V3
-# source: https://geoportal.statistics.gov.uk/datasets/middle-layer-super-output-areas-december-2011-boundaries-full-clipped-bfc-ew-v3
-msoa_2011 <-
-  read_sf("https://opendata.arcgis.com/datasets/1382f390c22f4bed89ce11f2a9207ff0_0.kml?outSR=%7B%22latestWkid%22%3A27700%2C%22wkid%22%3A27700%7D") %>%
+# Middle Layer Super Output Areas (December 2011) Boundaries Full Clipped (BFC) EW V3
+# source: https://geoportal.statistics.gov.uk/datasets/middle-layer-super-output-areas-december-2011-boundaries-full-clipped-bfc-ew-v3?geometry=-41.548%2C48.013%2C37.202%2C57.298
+GET(
+  "https://opendata.arcgis.com/datasets/1382f390c22f4bed89ce11f2a9207ff0_0.zip?outSR=%7B%22latestWkid%22%3A27700%2C%22wkid%22%3A27700%7D",
+  write_disk(tf <- tempfile(fileext = ".zip"))
+)
+
+unzip(tf, exdir = tempdir())
+unlink(tf)
+rm(tf)
+
+shapefile <- paste0(
+  tempdir(),
+  "/Middle_Layer_Super_Output_Areas__December_2011__Boundaries_Full_Clipped__BFC__EW_V3.shp"
+)
+
+msoa <-
+  read_sf(shapefile) %>%
   st_transform(crs = 4326)
 
 # Select and rename vars
-msoa_2011 <-
-  msoa_2011 %>%
+msoa <-
+  msoa %>%
   select(
-    msoa_name = Name,
-    msoa_code = Code,
+    msoa_name = MSOA11NM,
+    msoa_code = MSOA11CD,
     geometry
   )
 
 # Make sure geometries are valid
-msoa_2011 <- st_make_valid(msoa_2011)
+msoa <- st_make_valid(msoa)
 
 # Save output to data/ folder
-usethis::use_data(msoa_2011, overwrite = TRUE)
+# usethis::use_data(stp, overwrite = TRUE)
