51519795c0c4efd76b6d72c4000379e57efa3e68
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-02-12 14:28:49 +0100

    Update build method and checks

--- data-raw/counties-ua.R
+++ data-raw/counties-ua.R
@@ -1,28 +1,20 @@
-# ---- Load libraries ----
+# ---- Load ----
 library(tidyverse)
 library(sf)
-library(httr)
 library(lobstr)
+library(devtools)
 
-# ---- Load shapefile ----
-# Counties and Unitary Authorities (December 2019) Boundaries UK BUC - Ultra Generalised (500m) - clipped to the coastline (Mean High Water mark).
-# source: https://geoportal.statistics.gov.uk/datasets/counties-and-unitary-authorities-december-2019-boundaries-uk-buc?geometry=-39.482%2C47.293%2C38.609%2C56.716
-GET(
-  "https://opendata.arcgis.com/datasets/b216b4c8a4e74f6fb692a1785255d777_0.zip?outSR=%7B%22latestWkid%22%3A27700%2C%22wkid%22%3A27700%7D",
-  write_disk(tf <- tempfile(fileext = ".zip"))
-)
+# Load package
+load_all(".")
 
-unzip(tf, exdir = tempdir())
-unlink(tf)
-rm(tf)
-
-shapefile <- paste0(
-  tempdir(),
-  "/Counties_and_Unitary_Authorities_(December_2019)_Boundaries_UK_BUC.shp"
-)
+# Set query url
+query_url <-
+  query_urls %>%
+  filter(data_set == "counties_ua") %>%
+  pull(query_url)
 
 counties_ua <-
-  read_sf(shapefile) %>%
+  read_sf(query_url) %>%
   st_transform(crs = 4326)
 
 # Select and rename vars
@@ -37,10 +29,18 @@ counties_ua <-
 # Make sure geometries are valid
 counties_ua <- st_make_valid(counties_ua)
 
+# Check geometry types are homogenous
+if(counties_ua %>% st_geometry_type() %>% unique() != "MULTIPOLYGON"){
+  stop("Incorrect geometry types")
+}
+
 # Check object is below 50Mb GitHub warning limit
 if(obj_size(counties_ua) > 50000000) {
   stop("File is too large")
 }
 
+# Rename
+boundaries_counties_ua <- counties_ua
+
 # Save output to data/ folder
-usethis::use_data(counties_ua, overwrite = TRUE)
+usethis::use_data(boundaries_counties_ua, overwrite = TRUE)
