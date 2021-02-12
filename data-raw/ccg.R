ccbcb62f785f8997c49ce76b81233ff37140f6a9
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2021-02-12 13:08:23 +0100

    Update data set to use query url and rename output

--- data-raw/ccg.R
+++ data-raw/ccg.R
@@ -1,28 +1,20 @@
-# ---- Load libraries ----
+# ---- Load ----
 library(tidyverse)
 library(sf)
-library(httr)
 library(lobstr)
+library(devtools)
 
-# ---- Load shapefile ----
-# Clinical Commissioning Groups (April 2020) Ultra Generalised Boundaries EN
-# source: https://geoportal.statistics.gov.uk/datasets/clinical-commissioning-groups-april-2020-ultra-generalised-boundaries-en?geometry=-41.190%2C48.021%2C36.901%2C57.304
-GET(
-  "https://opendata.arcgis.com/datasets/dbfaf69873794690af4acddaf581572f_1.zip?outSR=%7B%22latestWkid%22%3A27700%2C%22wkid%22%3A27700%7D",
-  write_disk(tf <- tempfile(fileext = ".zip"))
-)
+# Load package
+load_all(".")
 
-unzip(tf, exdir = tempdir())
-unlink(tf)
-rm(tf)
-
-shapefile <- paste0(
-  tempdir(),
-  "/Clinical_Commissioning_Groups_(April_2020)_EN_BUC.shp"
-)
+# Set query url
+query_url <-
+  query_urls %>%
+  filter(data_set == "ccg") %>%
+  pull(query_url)
 
 ccg <-
-  read_sf(shapefile) %>%
+  read_sf(query_url) %>%
   st_transform(crs = 4326)
 
 # Select and rename vars
@@ -37,10 +29,18 @@ ccg <-
 # Make sure geometries are valid
 ccg <- st_make_valid(ccg)
 
+# Check geometry types are homogenous
+if(ccg %>% st_geometry_type() %>% unique() != "MULTIPOLYGON"){
+  stop("Incorrect geometry types")
+}
+
 # Check object is below 50Mb GitHub warning limit
 if(obj_size(ccg) > 50000000) {
   stop("File is too large")
 }
 
+# Rename for package
+boundaries_ccg <- ccg
+
 # Save output to data/ folder
-usethis::use_data(ccg, overwrite = TRUE)
+usethis::use_data(boundaries_ccg, overwrite = TRUE)
