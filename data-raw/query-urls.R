2d7067ed2bac4e42417e897b0deebae049b70853
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-02-23 10:46:08 +0100

    remove frequency and pub data cols

--- data-raw/query-urls.R
+++ data-raw/query-urls.R
@@ -7,11 +7,11 @@ query_urls <-
   tribble(
 
     # Column Names
-    ~geog_type, ~id, ~date, ~frequency, ~next_pub, ~license, ~query, ~source,
+    ~geog_type, ~id, ~date, ~license, ~query, ~source,
 
     # Boundaries
-    "boundaries", "countries_20", "01.12.20", NA, NA, "OGLv3", "https://opendata.arcgis.com/datasets/bb7104d3a9c04937be57e408288282dc_0.geojson", "https://geoportal.statistics.gov.uk/datasets/countries-december-2020-uk-buc/explore?location=54.650000C-3.250000%2C6.04",
-    "ccg", "ccg_20", "01.04.20"
+    "boundaries", "countries_20", "01.12.20", "OGLv3", "https://opendata.arcgis.com/datasets/bb7104d3a9c04937be57e408288282dc_0.geojson", "https://geoportal.statistics.gov.uk/datasets/countries-december-2020-uk-buc/explore?location=54.650000C-3.250000%2C6.04",
+    "ccg", "ccg_21", "01.04.21", "OGLv3"
 
     # Points
 
