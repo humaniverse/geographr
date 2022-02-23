70267adf598f2bd36776133a18b071cea7299782
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-02-23 12:38:27 +0100

    Reorder alphabetically

--- R/data.R
+++ R/data.R
@@ -1,19 +1,3 @@
-#' Countries (December 2020)
-#'
-#' A data set containing 2020 Country geometries and
-#' their associated names and codes. Boundaries are Ultra Generalised (500m) -
-#' clipped to the coastline (Mean High Water mark).
-#'
-#' @format A data frame of class "sf" with 4 rows and 3 variables:
-#' \describe{
-#'   \item{country_20_name}{country name}
-#'   \item{country_20_code}{Country code}
-#'   \item{geometry}{multipolygon coordinates}
-#'   ...
-#' }
-#' @source \url{https://geoportal.statistics.gov.uk/}
-"boundaries_countries_20"
-
 #' Clinical Commissioning Groups (April 2021)
 #'
 #' A data set containing 2021 Clinical Commissioning Group (CCG) geometries and
@@ -60,6 +44,22 @@
 #' }
 #' @source \url{https://geoportal.statistics.gov.uk/}
 "boundaries_counties_ua_21"
+
+#' Countries (December 2020)
+#'
+#' A data set containing 2020 Country geometries and
+#' their associated names and codes. Boundaries are Ultra Generalised (500m) -
+#' clipped to the coastline (Mean High Water mark).
+#'
+#' @format A data frame of class "sf" with 4 rows and 3 variables:
+#' \describe{
+#'   \item{country_20_name}{country name}
+#'   \item{country_20_code}{Country code}
+#'   \item{geometry}{multipolygon coordinates}
+#'   ...
+#' }
+#' @source \url{https://geoportal.statistics.gov.uk/}
+"boundaries_countries_20"
 
 #' Local Authority Districts (December 2019)
 #'
