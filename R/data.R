70d50393384ed1715047db1813fe0a858db301ce
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-02-23 11:19:58 +0100

    Add 2019 lad boundaries

--- R/data.R
+++ R/data.R
@@ -60,3 +60,19 @@
 #' }
 #' @source \url{https://geoportal.statistics.gov.uk/}
 "boundaries_counties_ua_21"
+
+#' Local Authority Districts (December 2019)
+#'
+#' A data set containing 2019 Local Authority Districts (LAD) geometries and
+#' their associated names and codes. Boundaries are Ultra Generalised (500m) -
+#' clipped to the coastline (Mean High Water mark).
+#'
+#' @format A data frame of class "sf" with 382 rows and 3 variables:
+#' \describe{
+#'   \item{lad_19_name}{LAD name}
+#'   \item{lad_19_code}{LAD code}
+#'   \item{geometry}{multipolygon coordinates}
+#'   ...
+#' }
+#' @source \url{https://geoportal.statistics.gov.uk/}
+"boundaries_lad_19"
