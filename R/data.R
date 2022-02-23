c2bb6b7ea31593b29a1ee71dfe88c07239b7439b
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-02-23 11:24:05 +0100

    Add 2021 lad boundaries

--- R/data.R
+++ R/data.R
@@ -76,3 +76,19 @@
 #' }
 #' @source \url{https://geoportal.statistics.gov.uk/}
 "boundaries_lad_19"
+
+#' Local Authority Districts (December 2021)
+#'
+#' A data set containing 2021 Local Authority Districts (LAD) geometries and
+#' their associated names and codes. Boundaries are Ultra Generalised (500m) -
+#' clipped to the coastline (Mean High Water mark).
+#'
+#' @format A data frame of class "sf" with 374 rows and 3 variables:
+#' \describe{
+#'   \item{lad_21_name}{LAD name}
+#'   \item{lad_21_code}{LAD code}
+#'   \item{geometry}{multipolygon coordinates}
+#'   ...
+#' }
+#' @source \url{https://geoportal.statistics.gov.uk/}
+"boundaries_lad_21"
---
+++ data-raw/boundaries_lad_21.R
@@ -0,0 +1,55 @@
+# ---- Load ----
+library(tidyverse)
+library(sf)
+library(lobstr)
+library(devtools)
+
+# Load package
+load_all(".")
+
+# Set query url
+query_url <-
+  query_urls %>%
+  filter(id == "lad_21") %>%
+  pull(query)
+
+lad <-
+  read_sf(query_url) %>%
+  st_transform(crs = 4326)
+
+# Select and rename vars
+lad <-
+  lad %>%
+  select(
+    lad_21_name = LAD21NM,
+    lad_21_code = LAD21CD,
+    geometry
+  )
+
+# Make sure geometries are valid
+lad <- st_make_valid(lad)
+
+# Check geometry types are homogenous
+if (lad %>%
+  st_geometry_type() %>%
+  unique() %>%
+  length() > 1) {
+  stop("Incorrect geometry types")
+}
+
+if (lad %>%
+  st_geometry_type() %>%
+  unique() != "MULTIPOLYGON") {
+  stop("Incorrect geometry types")
+}
+
+# Check object is below 50Mb GitHub warning limit
+if (obj_size(lad) > 50000000) {
+  stop("File is too large")
+}
+
+# Rename
+boundaries_lad_21 <- lad
+
+# Save output to data/ folder
+usethis::use_data(boundaries_lad_21, overwrite = TRUE)
