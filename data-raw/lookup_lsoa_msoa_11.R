efd23ce88fc7cee4a993b6a3ee5ea2753bf5c7d0
Aileen McDonald <aileen_mcdonald14@hotmail.com>
2022-03-07 19:14:26 +0000

    lookups

---
+++ data-raw/lookup_lsoa_msoa_11.R
@@ -0,0 +1,32 @@
+# ---- Load ----
+library(tidyverse)
+library(devtools)
+
+# Load package
+load_all(".")
+
+# Set query url
+query_url <-
+  query_urls |>
+  filter(id == "lsoa_msoa_11") |>
+  pull(query)
+
+lsoa_msoa <-
+  read_csv(query_url)
+
+# Select and rename vars
+lsoa_msoa <-
+  lsoa_msoa |>
+  select(
+    lsoa_11_name = LSOA11NM,
+    lsoa_11_code = LSOA11CD,
+    msoa_11_name = MSOA11NM,
+    msoa_11_code = MSOA11CD
+  ) |>
+  distinct()
+
+# Rename
+lookup_lsoa_msoa_11 <- lsoa_msoa
+
+# Save output to data/ folder
+usethis::use_data(lookup_lsoa_msoa_11, overwrite = TRUE)
