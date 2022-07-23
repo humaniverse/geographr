4a63ebd946ce8a2ba7b2f7436b879cfc80493cd6
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2022-07-23 14:52:17 +0100

    Added function to calculate proportions

--- R/calculate.R
+++ R/calculate.R
@@ -1,4 +1,4 @@
-#' Calculate the 'extent' scores when aggreating up small areas
+#' Calculate the 'extent' scores when aggregating up small areas
 #'
 #' "Extent" is the proportion of the local population that live in areas
 #' classified as among the most deprived in the higher geography. The
@@ -12,12 +12,12 @@
 #' (0.95-0.05)/19 for each of the subsequent nineteen percentiles until it
 #' reaches 0.05 for the most deprived thirtieth percentile, and zero for areas
 #' outside the most deprived 30 per cent"
-#' 
+#'
 #' The direction of the scale of data inputted by the functioin always matches
 #' that of the data outputted. For example if data is inputted into the function
 #' where high scores equals high vulnerability, the outputted data set will hold
 #' this relationship true.
-#' 
+#'
 #' @param data Data frame containing a variable to be aggregated, lower level
 #'        geography population estimates, and a higher level geographical
 #'        grouping variable
@@ -34,9 +34,9 @@
 #' and this is where the weighting should be focused. For indicators like
 #' 'Physical Activity' and 'Bed Availability' it should be set to FALSE. This is
 #' because lower values in these outcomes indicate worse outcomes (higher
-#' vulnerability and lower capacity) and this is where the weighting should be 
+#' vulnerability and lower capacity) and this is where the weighting should be
 #' focused.
-#' 
+#'
 #' @export
 calculate_extent <-
   function(data,
@@ -76,4 +76,35 @@ calculate_extent <-
     }
 
     return(data)
+  }
+
+#' Calculate proportion of small areas in the higher-level geography that are
+#' within the 10% most deprived areas in the nation.
+#'
+#' @param data Data frame containing a variable to be aggregated, lower level
+#'        geography population estimates, and a higher level geographical
+#'        grouping variable
+#' @param var Name of the variable in the data frame containing the variable to
+#'        be aggregated (e.g. decile) for the lower level geography
+#' @param higher_level_geography Name of the variable in the data frame
+#'        containing the higher level geography names/codes
+#' @param max_quantile Get proportion of small areas categorised as less than
+#'        or equal to `max_quantile` (default = 1)
+#'
+#' @importFrom rlang .data
+#' @export
+calculate_proportion <-
+  function(data,
+           var,
+           higher_level_geography,
+           max_quantile = 1) {
+    data |>
+      # Label LSOAs by whether they're in top 10% most-deprived then summarise by this label
+      dplyr::mutate(Top10 = ifelse({{ var }} <= max_quantile, "Top10", "Other")) |>
+      janitor::tabyl({{higher_level_geography}}, .data$Top10) |>
+
+      # Calculate proportion of most deprived LSOAs
+      dplyr::mutate(Proportion = .data$Top10 / (.data$Top10 + .data$Other)) |>
+      dplyr::select({{ higher_level_geography }}, .data$Proportion) |>
+      tibble::as_tibble()
   }
