54554c657ecd6110923b6be9089f260a60f456ae
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2024-12-10 16:05:25 +0000

    Add LTLA 2024 boundaries

--- R/data.R
+++ R/data.R
@@ -184,6 +184,22 @@
 #' @source \url{https://geoportal.statistics.gov.uk/}
 "boundaries_ltla21"
 
+#' Local Authority Districts (July 2024)
+#'
+#' A data set containing 2024 Local Authority Districts (LAD) geometries and
+#' their associated names and codes. Boundaries are Ultra Generalised (500m) -
+#' clipped to the coastline (Mean High Water mark).
+#'
+#' @format A data frame of class "sf" with 361 rows and 3 variables:
+#' \describe{
+#'   \item{ltla24_name}{LAD name}
+#'   \item{ltla24_code}{LAD code}
+#'   \item{geometry}{multipolygon coordinates}
+#'   ...
+#' }
+#' @source \url{https://geoportal.statistics.gov.uk/}
+"boundaries_ltla24"
+
 #' Middle Layer Super Output Areas (2011)
 #'
 #' A data set containing 2011 Middle Super Output Area (MSOA) geometries and
